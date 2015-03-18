1eca65ee7562a65c39e92cfb69c879cd291c928c
obandovic <obandovic@gmail.com>
2015-03-18 12:10:30 -0500

    update

--- Cyberoam/cyberoam.sql
+++ Cyberoam/cyberoam.sql
@@ -17,8 +17,8 @@ INSERT IGNORE INTO plugin (id, type, name, description) VALUES
 (10091, 1, 'Cyberoam UTM', 'Cyberoam UTM');
 
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES 
-(1001, 10091, NULL, NULL, 'Cyberoam Content Filtering', , ),
-(1002, 10091, NULL, NULL, 'Cyberoam Firewall Event', , ),
-(1003, 10091, NULL, NULL, 'Cyberoam IDP Event', , ),
-(1004, 10091, NULL, NULL, 'Cyberoam Generic Event', , ),
+(10091, 1001, NULL, NULL, 'Cyberoam Content Filtering', , ),
+(10091, 1002, NULL, NULL, 'Cyberoam Firewall Event', , ),
+(10091, 1003, NULL, NULL, 'Cyberoam IDP Event', , ),
+(10091, 1004, NULL, NULL, 'Cyberoam Generic Event', , ),
 ;
