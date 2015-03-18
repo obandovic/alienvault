fb03fbaa0a64bb21b5c5b13b5addd78acaf98561
obandovic <obandovic@gmail.com>
2015-03-18 12:21:16 -0500

    updates

--- Cyberoam/cyberoam.sql
+++ Cyberoam/cyberoam.sql
@@ -13,12 +13,10 @@ DELETE FROM plugin_sid where plugin_id = "10091";
 
 INSERT IGNORE INTO software_cpe (cpe, name, version, line, vendor, plugin) VALUES ('cpe:/o:CYBEROAM:FIREWALL:-', 'UTM','UTM','CYBEROAM UTM','CYBEROAM', 'cyberoam:10091');
 
-INSERT IGNORE INTO plugin (id, type, name, description) VALUES 
-(10091, 1, 'Cyberoam UTM', 'Cyberoam UTM');
+INSERT IGNORE INTO plugin (id, type, name, description) VALUES (10091, 1, 'Cyberoam UTM', 'Cyberoam UTM');
 
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES 
-(10091, 1001, NULL, NULL, 'Cyberoam Content Filtering', , ),
-(10091, 1002, NULL, NULL, 'Cyberoam Firewall Event', , ),
-(10091, 1003, NULL, NULL, 'Cyberoam IDP Event', , ),
-(10091, 1004, NULL, NULL, 'Cyberoam Generic Event', , ),
-;
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (10091, 1001, 3, 121, 'Cyberoam Content Filtering');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (10091, 1002, 3, 121, 'Cyberoam Firewall Event');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (10091, 1003, 3, 121, 'Cyberoam IDP Event');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (10091, 1004, 3, 121, 'Cyberoam Generic Event');
+
