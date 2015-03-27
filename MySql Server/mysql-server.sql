9f8bc966a158883b21234323e2fba83490f800ce
obandovic <obandovic@gmail.com>
2015-03-27 15:20:57 -0500

    update

--- MySql Server/mysql-server.sql
+++ MySql Server/mysql-server.sql
@@ -3,10 +3,8 @@
 -- Author: VICTOR OBANDO
 -- www.alienvault.com
 -- Last Modified: 3/27/2015
-
-
 DELETE FROM plugin WHERE id = '9201';
 DELETE FROM plugin_sid WHERE plugin_id = '9201';
 
-INSERT IGNORE INTO plugin (id, type, name, description, vendor) VALUES (9201, 1, ‘MySQLServer', 'MySQL Server', 'Oracle');
+INSERT IGNORE INTO plugin (id, type, name, description, vendor) VALUES (9201, 1, 'mysqlserver', 'MySQL Server', 'Oracle');
 INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (9201, 1, NULL, NULL, 'MySQL Server Event');
