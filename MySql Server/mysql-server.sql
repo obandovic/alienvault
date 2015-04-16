79669700d71cc300a88fb83a47e3f0513135f2c8
obandovic <obandovic@gmail.com>
2015-04-16 15:22:06 -0500

    upates

--- MySql Server/mysql-server.sql
+++ MySql Server/mysql-server.sql
@@ -2,9 +2,14 @@
 -- plugin_id: 9201
 -- Author: VICTOR OBANDO
 -- www.alienvault.com
--- Last Modified: 3/27/2015
-DELETE FROM plugin WHERE id = '9201';
-DELETE FROM plugin_sid WHERE plugin_id = '9201';
+-- Last Modified: 4/14/2015
 
-INSERT IGNORE INTO plugin (id, type, name, description, vendor) VALUES (9201, 1, 'mysqlserver', 'MySQL Server', 'Oracle');
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (9201, 1, NULL, NULL, 'MySQL Server Event');
+DELETE FROM plugin WHERE id = '9201';
+DELETE FROM plugin_sid where plugin_id = '9201';
+
+
+INSERT IGNORE INTO plugin (id, type, name, description, vendor) VALUES (9201, 1, 'MySql', 'MySql Database', 'Oracle');
+
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (9201, 100, NULL, NULL, 'MySql Server: UPDATE Command Executed');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (9201, 101, NULL, NULL, 'MySql Server: INSERT Command Executed');
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name) VALUES (9201, 102, NULL, NULL, 'MySql Server: DELETE Command Executed');
