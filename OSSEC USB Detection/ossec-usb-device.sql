dfd089f53a252d4a5bcc7750731b1c81c7548857
obandovic <obandovic@gmail.com>
2015-11-20 14:47:51 -0600

    USB Devices.

---
+++ OSSEC USB Detection/ossec-usb-device.sql
@@ -0,0 +1,15 @@
+-- USB DEVICE DETECTION
+-- plugin_id: 9271
+-- Author: VICTOR OBANDO
+-- www.alienvault.com
+-- Last Modified: 11/19/15
+
+
+DELETE FROM plugin WHERE id = "9271";
+DELETE FROM plugin_sid where plugin_id = "9271";
+
+
+INSERT IGNORE INTO plugin (id, type, name, description, product_type, vendor) VALUES (9271, 1, 'USB DEVICES', 'USB STORAGE DEVICE ACTIVITY', 20, 'ALIENVAULT');
+
+
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, class_id, name, priority, reliability) VALUES (9271, 100051, 11, NULL, 'USB STORAGE DEVICE DETECTED', 5, 5);
