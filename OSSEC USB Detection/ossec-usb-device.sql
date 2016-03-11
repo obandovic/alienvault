88d9e9c171a441f67a8a8770290f6acc9ef48558
obandovic <obandovic@gmail.com>
2016-03-10 18:52:59 -0600

    UPDATE

--- OSSEC USB Detection/ossec-usb-device.sql
+++ OSSEC USB Detection/ossec-usb-device.sql
@@ -12,4 +12,4 @@ DELETE FROM plugin_sid where plugin_id = "9271";
 INSERT IGNORE INTO plugin (id, type, name, description, product_type, vendor) VALUES (9271, 1, 'USB DEVICES', 'USB STORAGE DEVICE ACTIVITY', 20, 'ALIENVAULT');
 
 
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, name, priority, reliability) VALUES (9271, 100051, 11, 138, 'USB STORAGE DEVICE ACTIVITY', 5, 5);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, name, priority, reliability) VALUES (9271, 100051, 11, 138, 'USB STORAGE DEVICE ACTIVITY', 2, 2);
